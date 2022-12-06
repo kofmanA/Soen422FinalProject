ed87729e0c31d1babf396d4d271674ac3fbe0dfe
VikaTheDuck <46077629+VikaTheDuck@users.noreply.github.com>
2022-12-06 12:56:41 -0500

    Comment

--- mic.ino
+++ mic.ino
@@ -1,3 +1,6 @@
+// can use the analog or digital values to determine if a certain decibel treshold has been reached
+
+
 int sound_digital = 0;
 int sound_analog = 4;
 
@@ -16,7 +19,7 @@ void loop(){
   Serial.print("\t");
   Serial.println(val_digital);
 
-  if (val_analog >= 2165)
+  if (val_analog >= 2165) // checking if the analog value is over 2165 (decibels i think?)
   {
     digitalWrite (LED_BUILTIN, HIGH);
     delay(3000);
