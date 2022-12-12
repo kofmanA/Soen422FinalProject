19e841d520d7ca9b371a3ed3337b85f095ed777c
-alex_kofman- <akofman123@gmail.com>
2022-12-12 16:22:22 -0500

    Add temp working code

--- mic/mic.ino
+++ mic/mic.ino
@@ -2,31 +2,39 @@
 
 
 int sound_digital = 0;
-int sound_analog = 4;
+
+//int sound_analog = 4;
+int sound_analog = 37;
+
 
 void setup(){
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(sound_analog,INPUT);
   pinMode(sound_digital, INPUT);  
 }
 
+int redButtonPin = 2;
+int yellowButtonPin = 19;
+int greenButtonPin = 13;
+int blueButtonPin = 12;
+
+
 void loop(){
-  digitalWrite (led, HIGH);
   int val_digital = digitalRead(sound_digital);
   int val_analog = analogRead(sound_analog);
 
   Serial.print(val_analog);
-  Serial.print("\t");
+  Serial.print("\n");
   Serial.println(val_digital);
 
-  if (val_analog >= 2165) // checking if the analog value is over 2165 (decibels i think?)
+  if ( val_analog >= 30 ) // checking if the analog value is over 2165 (decibels i think?)
   {
     digitalWrite (LED_BUILTIN, HIGH);
-    delay(3000);
     Serial.println("Game started!");
-    }
+  }
   else
   {
     digitalWrite (LED_BUILTIN, LOW);
-    }
+  }
 }
